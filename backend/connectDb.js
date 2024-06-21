0256fa3f8d29829a5dc5a1d2b4aff1f997408b53
Vaish Raja <varish.mtalkz@gmail.com>
2024-06-21 17:55:21 +0530

    db_urlChanges

--- backend/connectDb.js
+++ backend/connectDb.js
@@ -3,8 +3,8 @@ import { connect } from 'mongoose'
 const ConnectDb = async () => {
 
     try {
-        // const db = await connect(process.env.DB_URL)
-        const db=await connect('mongodb://0.0.0.0:27017/webApp')
+        const db = await connect(process.env.DB_URL)
+        // const db=await connect('mongodb://0.0.0.0:27017/webApp')
         console.log("connect DB")
     } catch (err) {
         console.log(`error ${err.message}`)
