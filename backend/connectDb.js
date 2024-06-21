f5f8426fe9672f50bfc2349a1feb1681a43d9027
Vaish Raja <varish.mtalkz@gmail.com>
2024-06-21 17:53:47 +0530

    FinalSubmission

---
+++ backend/connectDb.js
@@ -0,0 +1,16 @@
+import { connect } from 'mongoose'
+
+const ConnectDb = async () => {
+
+    try {
+        // const db = await connect(process.env.DB_URL)
+        const db=await connect('mongodb://0.0.0.0:27017/webApp')
+        console.log("connect DB")
+    } catch (err) {
+        console.log(`error ${err.message}`)
+    }
+}
+
+ConnectDb()
+
+
